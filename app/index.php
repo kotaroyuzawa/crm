5f3fda09ccc05893cbc1ca979cf615f93f54e13a
Nikita-bot-web <plaksienko.nikita@gmail.com>
2023-06-22 20:12:04 +0200

    Revert "Adde Offer Prototype"
    This reverts commit b275fe47433d901cf960255ea34feb33ee5d0ec3.

--- app/index.php
+++ app/index.php
@@ -1,13 +1,11 @@
 <?php
 
 use App\Inc\App;
-use App\inc\Offers\OffersView;
 
 require_once 'autoloader.php';
 require_once 'config.php';
 
 $app = new App();
-
 
 /* placeholder
 $app->addRoute('URL', 'METHOD', function() {
@@ -15,14 +13,4 @@ $app->addRoute('URL', 'METHOD', function() {
 });
 */
 
-$app->addRoute('/offers', 'GET', function() {
-
-    echo 123455;
-    echo (new OffersView())->render();
-
-});
-
-
-
 $app->run();
-
