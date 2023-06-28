8036b348cf7f0b07c7ad950904853f29f5c52cf6
KotaroYuzawa <ktr.yzw@gmail.com>
2023-06-28 10:09:11 +0200

    customer view edited

--- app/src/Templates/customers/addCustomer.php
+++ app/src/Templates/customers/addCustomer.php
@@ -1,30 +1,30 @@
 <form action="/customers/add" method="post">
     <div class="form-group">
-        <label for="customer_name">Customer Name</label>
+        <label for="customerName">Customer Name</label>
         <input type="text" class="form-control" name="customerName" id="customerName" aria-describedby="emailHelp" required>
     </div>
     <div class="form-group">
-        <label for="customer_street">Customer Street</label>
+        <label for="customerStreet">Customer Street</label>
         <input type="text" class="form-control" name="customerStreet" id="customerStreet" required>
     </div>
     <div class="form-group">
-        <label for="customer_street_nr">Customer Street Nr</label>
+        <label for="customerStreetNr">Customer Street Nr</label>
         <input type="text" class="form-control" name="customerStreetNr" id="customerStreetNr" required>
     </div>
     <div class="form-group">
-        <label for="customer_street_additional">Customer Street Additional</label>
+        <label for="customerStreetAdditional">Customer Street Additional</label>
         <input type="text" class="form-control" name="customerStreetAdditional" id="customerStreetAdditional">
     </div>
     <div class="form-group">
-        <label for="customer_city">Customer City</label>
+        <label for="customerCity">Customer City</label>
         <input type="text" class="form-control" name="customerCity" id="customerCity" required>
     </div>
     <div class="form-group">
-        <label for="customer_email">Customer Email</label>
+        <label for="customerEmail">Customer Email</label>
         <input type="email" class="form-control" name="customerEmail" id="customerEmail" required>
     </div>
     <div class="form-group">
-        <label for="customer_phone">Customer Phone</label>
+        <label for="customerPhone">Customer Phone</label>
         <input type="text" class="form-control" name="customerPhone" id="customerPhone" required>
     </div>
 
