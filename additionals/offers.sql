2360764c159f4d3fcda83d31f455240231257b45
Nikita-bot-web <plaksienko.nikita@gmail.com>
2023-06-30 09:00:45 +0200

    Add the right offer SQL

--- additionals/offers.sql
+++ additionals/offers.sql
@@ -1,18 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
--- Tabellenstruktur für Tabelle `offers`
+-- Host: db:3306
+-- Generation Time: Jun 30, 2023 at 06:59 AM
+-- Server version: 11.0.2-MariaDB-1:11.0.2+maria~ubu2204
+-- PHP Version: 8.1.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
-CREATE TABLE `crm`.`offers` (
-                             `offer_id` INT NOT NULL AUTO_INCREMENT,
-                             `customer_id` INT,
-                             `created_at` DATETIME NOT NULL,
-                             `deleted_at` DATETIME NOT NULL,
-                             `updated_at` DATETIME NOT NULL,,
-                             `status` ENUM('Active','Inactive') NOT NULL DEFAULT 'Active',
-                             `sum` INT
+-- Database: `crm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `offers`
+--
+
+CREATE TABLE `offers` (
+                          `offer_id` int(11) NOT NULL,
+                          `customer_id` int(11) NOT NULL,
+                          `created_at` datetime NOT NULL,
+                          `deleted_at` datetime NOT NULL,
+                          `updated_at` datetime NOT NULL,
+                          `status` enum('Active','Inactive') NOT NULL DEFAULT 'Inactive',
+                          `sum` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Daten für Tabelle `offers`
+-- Dumping data for table `offers`
 --
-INSERT INTO `offers` (`offer_id`, `customer_id`, `created_at`, `deleted_at`, `updated_at`, `status`, `sum`) VALUES ('2', '1', '2023-06-26 17:06:16.000000', '2023-06-26 17:06:16.000000', '2023-06-26 17:06:16.000000', 'Inactive', '10000');
 
+INSERT INTO `offers` (`offer_id`, `customer_id`, `created_at`, `deleted_at`, `updated_at`, `status`, `sum`) VALUES
+                                                                                                                (1, 1, '2023-06-26 16:53:59', '2023-06-26 16:53:59', '2023-06-26 16:53:59', 'Inactive', 20000),
+                                                                                                                (6, 2, '2023-06-28 20:12:41', '2023-06-28 20:12:41', '2023-06-28 20:12:41', 'Inactive', 345);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `offers`
+--
+ALTER TABLE `offers`
+    ADD PRIMARY KEY (`offer_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `offers`
+--
+ALTER TABLE `offers`
+    MODIFY `offer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
