f644d959469fa30236c5d07077a7c2ca721a756d
Daher Al Mohamad Al Khalaf <daheralkhalaf@gmail>
2023-07-04 09:31:53 +0200

    Fix the position of the navigation in Company profile

--- app/src/Templates/company/main.php
+++ app/src/Templates/company/main.php
@@ -4,17 +4,17 @@
  */
 ?>
 <div class="container">
-    <div class="HeaderBtn">
+    <div class="">
         <nav class="navbar navbar-expand-lg navbar-light">
             <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a id="profile" class="nav-link" href="company">Profil</a>
+                <li class="nav-item pe-2">
+                    <a id="profile" class="btn btn-outline-primary" href="company">Profil</a>
                 </li>
                 <li class="nav-item">
-                    <a id="profileEdit" class="nav-link" href="company/edit">Profil bearbeiten</a>
+                    <a id="profileEdit" class="btn btn-outline-primary" href="company/edit">Profil bearbeiten</a>
                 </li>
             </ul>
-        </nav
+        </nav>
     </div>
     <?= $content ?>
 </div>
