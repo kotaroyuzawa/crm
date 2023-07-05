113861b2dc0f303ea7528a7f5dce7e1315e5e316
Jan von pirch <janvonpirch21@gmail.com>
2023-07-05 09:32:17 +0200

    Update offers table

--- additionals/offers.sql
+++ additionals/offers.sql
@@ -3,69 +3,58 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db:3306
--- Generation Time: Jun 30, 2023 at 06:59 AM
--- Server version: 11.0.2-MariaDB-1:11.0.2+maria~ubu2204
--- PHP Version: 8.1.17
+-- Erstellungszeit: 05. Jul 2023 um 07:31
+-- Server-Version: 10.11.2-MariaDB-1:10.11.2+maria~ubu2204
+-- PHP-Version: 8.1.17
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
--- Database: `crm`
+-- Datenbank: `crm`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `offers`
+-- Tabellenstruktur für Tabelle `offers`
 --
 
 CREATE TABLE `offers` (
                           `offer_id` int(11) NOT NULL,
-                          `customer_id` int(11) NOT NULL,
-                          `created_at` datetime NOT NULL,
-                          `deleted_at` datetime NOT NULL,
-                          `updated_at` datetime NOT NULL,
-                          `status` enum('Active','Inactive') NOT NULL DEFAULT 'Inactive',
-                          `sum` int(11) NOT NULL
+                          `customer_id` int(11) DEFAULT NULL,
+                          `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+                          `deleted_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+                          `updated_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp(),
+                          `status` enum('Active','Inactive') NOT NULL DEFAULT 'Active',
+                          `sum` int(11) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `offers`
+-- Daten für Tabelle `offers`
 --
 
 INSERT INTO `offers` (`offer_id`, `customer_id`, `created_at`, `deleted_at`, `updated_at`, `status`, `sum`) VALUES
-                                                                                                                (1, 1, '2023-06-26 16:53:59', '2023-06-26 16:53:59', '2023-06-26 16:53:59', 'Inactive', 20000),
-                                                                                                                (6, 2, '2023-06-28 20:12:41', '2023-06-28 20:12:41', '2023-06-28 20:12:41', 'Inactive', 345);
+    (2, 1, '2023-06-26 17:06:16', '2023-06-26 17:06:16', '2023-06-26 17:06:16', 'Inactive', 10000);
 
 --
--- Indexes for dumped tables
+-- Indizes der exportierten Tabellen
 --
 
 --
--- Indexes for table `offers`
+-- Indizes für die Tabelle `offers`
 --
 ALTER TABLE `offers`
     ADD PRIMARY KEY (`offer_id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT für exportierte Tabellen
 --
 
 --
--- AUTO_INCREMENT for table `offers`
+-- AUTO_INCREMENT für Tabelle `offers`
 --
 ALTER TABLE `offers`
-    MODIFY `offer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+    MODIFY `offer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
